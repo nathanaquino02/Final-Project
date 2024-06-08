2972e17688ce5860d1ff0d4a8a6b2bbd1ca665c9
Nathan Aquino <nbaquino02@gmail.com>
2024-06-08 20:52:52 +0800

    fixed quicksort

--- src/LinkSortAlgo.java
+++ src/LinkSortAlgo.java
@@ -1,3 +1,145 @@
+import java.util.Arrays;
+
 public class LinkSortAlgo {
 
+    //Merge Sort
+    // MergeSort
+    public static void merge(int arr[], int l, int m, int r) {
+        // Find sizes of two subarrays to be merged
+        int n1 = m - l + 1;
+        int n2 = r - m;
+        // Create temp arrays
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+        // Copy data to temp arrays
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
+        // Merge the temp arrays
+        // Initial indices of first and second subarrays
+        int i = 0, j = 0;
+        // Initial index of merged subarray array
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+        // Copy remaining elements of L[] if any
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+        // Copy remaining elements of R[] if any
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    // Main function that sorts arr[l..r] using merge()
+    public static void mergeSort(int arr[], int l, int r) {
+        if (l < r) {
+            // Find the middle point
+            int m = l + (r - l) / 2;
+            // Sort first and second halves
+            mergeSort(arr, l, m);
+            mergeSort(arr, m + 1, r);
+            // Merge the sorted halves
+            merge(arr, l, m, r);
+        }
+    }
+
+    // Radix Sort
+    // A utility function to get maximum value in arr[]
+    static int getMax(int arr[], int n) {
+        int mx = arr[0];
+        for (int i = 1; i < n; i++)
+            if (arr[i] > mx)
+                mx = arr[i];
+        return mx;
+    }
+
+    // A function to do counting sort of arr[] according to the digit represented by exp.
+    static void countSort(int arr[], int n, int exp) {
+        int output[] = new int[n]; // output array
+        int i;
+        int count[] = new int[10];
+        Arrays.fill(count, 0);
+        // Store count of occurrences in count[]
+        for (i = 0; i < n; i++)
+            count[(arr[i] / exp) % 10]++;
+        // Change count[i] so that count[i] now contains actual position of this digit in output[]
+        for (i = 1; i < 10; i++)
+            count[i] += count[i - 1];
+        // Build the output array
+        for (i = n - 1; i >= 0; i--) {
+            output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+            count[(arr[i] / exp) % 10]--;
+        }
+        // Copy the output array to arr[], so that arr[] now contains sorted numbers according to
+        // current digit
+        for (i = 0; i < n; i++)
+            arr[i] = output[i];
+    }
+
+    // The main function to that sorts arr[] of size n using Radix Sort
+    static void radixSort(int arr[], int n) {
+        // Find the maximum number to know number of digits
+        int m = getMax(arr, n);
+        // Do counting sort for every digit. Note that instead of passing digit number, exp is
+        // passed. exp is 10^i where i is current digit number
+        for (int exp = 1; m / exp > 0; exp *= 10)
+            countSort(arr, n, exp);
+    }
+
+    // QuickSort
+    // A utility function to swap two elements
+    static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    // This function takes last element as pivot, places the pivot element at its correct
+    // position in sorted array, and places all smaller to left of pivot and all greater elements
+    // to right of pivot
+    static int partition(int[] arr, int low, int high) {
+        // Choosing the pivot
+        int pivot = arr[high];
+        // Index of smaller element and indicates the right position of pivot found so far
+        int i = (low - 1);
+        for (int j = low; j <= high - 1; j++) {
+            // If current element is smaller than the pivot
+            if (arr[j] < pivot) {
+                // Increment index of smaller element
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, high);
+        return (i + 1);
+    }
+
+    // The main function that implements QuickSort
+    // arr[] --> Array to be sorted,
+    // low --> Starting index,
+    // high --> Ending index
+    static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            // pi is partitioning index, arr[p] is now at right place
+            int pi = partition(arr, low, high);
+            // Separately sort elements before partition and after partition
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
 }
